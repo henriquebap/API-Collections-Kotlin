6f53fd485c58f8e2d767e351e0d67c54fa937ae4
unknown <henriquebaptista2003@gmail.com>
2021-07-05 19:44:15 -0300

    Improving Teste Operecoes class with Arrays count, Find & any

--- src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -12,8 +12,18 @@ fun main(){
     println("Menor Salarios: ${salarios.minOrNull()}")
     println("Media Salarial: ${salarios.average()}")
 
-    val salariosMenor2500 = salarios.filter { it > 2500.0 }
+    val salariosMenor2500 = salarios.filter { it < 2500.0 }
     println("-----------------------")
-    salariosMenor2500.forEach{ println(it)}
+    salariosMenor2500.forEach{ println("Esses sao os salarios maiores do que 2500 : " + it)}
 
+    println("-----------------------")
+    println("Existem " + salarios.count{ it in 200.0..10000.0} + " Salarios entre 200 - 10000")
+
+    println("-----------------------")
+    println(salarios.find { it == 2000.0 })
+    println(salarios.find { it == 5000.0 })
+
+    println("-----------------------")
+    println(salarios.any{ it == 100.0})
+    println(salarios.any{ it == 10055.0})
 }
